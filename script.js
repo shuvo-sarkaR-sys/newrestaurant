3e7ac86da3e4c43ded9db6ea4e144548693d3ed1
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-08-05 23:16:37 +0600

    upd

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const navLinks = document.getElementById('nav-links2');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+ const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
